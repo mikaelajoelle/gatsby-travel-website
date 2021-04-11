9209a4f1a5b337f76ed33cf2027d595cb6b1d63b
mikaelajoelle <74077372+mikaelajoelle@users.noreply.github.com>
2021-04-11 16:23:18 -0700

    adding images, creating graphql image query and image styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
-import * as React from "react"
-import HeroSection from "../components/HeroSection"
+import * as React from "react";
+import HeroSection from "../components/HeroSection";
+import Image from "../components/image";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <HeroSection />
+    <Image />
   </Layout>
 )
 
