1fc37adcdd559160b1fb998d3c9504e342adfc1c
mikaelajoelle <74077372+mikaelajoelle@users.noreply.github.com>
2021-04-11 21:51:01 -0700

    Footer and Footer Elements styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,33 +5,20 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-//import { useStaticQuery, graphql } from "gatsby"
-import "./layout.css"
+import * as React from "react";
+import "./layout.css";
 import Navbar from './Navbar';
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
-  /*const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)*/
 
   return (
     <>
     <Navbar />
         <main>{children}</main>
+    <Footer />
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
