0e9bd96c310e8e77c60ce00af912b411518cacdd
mikaelajoelle <74077372+mikaelajoelle@users.noreply.github.com>
2021-04-14 13:22:32 -0700

    creating about grid component and styling

--- src/components/AboutGrid.js
+++ src/components/AboutGrid.js
@@ -1,10 +1,59 @@
 import React from 'react';
 import styled from "styled-components";
+import Img from "gatsby-image";
+import { useStaticQuery, graphql } from "gatsby";
 
 const AboutGrid = ({heading}) => {
+
+    const data = useStaticQuery(graphql`
+    query AboutQuery {
+        allAboutDataJson {
+          edges {
+            node {
+              alt
+              title
+              desc
+              img {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `)
+
+     // Function goes through JSON data and returns data
+     function getSteps(data) {
+        const stepsArray = [];
+        data.allAboutDataJson.edges.forEach((item, index) => {
+            stepsArray.push(
+                    <StepContainer key={index}>
+                        <TextContent>
+                            <Heading>{item.node.title}</Heading>
+                            <Description>{item.node.desc}</Description>
+                        </TextContent>
+                        <ImgWrapper>
+                            <ImgContent 
+                                alt={item.node.alt}
+                                fluid={item.node.img.childImageSharp.fluid}> 
+                            </ImgContent>
+                        </ImgWrapper>
+                    </StepContainer>
+            )
+        })
+        return stepsArray
+     }
+
     return (
         <AboutContainer>
             <AboutHeading>{heading}</AboutHeading>
+            <AboutWrapper>
+                {getSteps(data)}
+            </AboutWrapper>
         </AboutContainer>
     )
 }
@@ -31,5 +80,64 @@ const AboutHeading = styled.h2`
         width: 60px;
         display: block;
         margin: 1rem auto 0 auto;
+    }
+`
+const AboutWrapper = styled.div`
+    padding: 0 10rem;
+    
+    @media screen and (max-width: 900px){
+        padding: 0 4rem;
+    }
+
+    @media screen and (max-width: 600px){
+        padding: 0 3rem;
+    }
+`
+
+const StepContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1.2fr .8fr;
+    grid-template-rows: 325px;
+    grid-gap: 2rem;
+    padding: 6rem 0;
+    color: #000;
+
+    @media screen and (max-width: 1100px){
+        grid-template-columns: 1fr;
+        grid-template-rows: 200px;
+        padding: 5rem 0;
+    }
+`
+
+const TextContent = styled.div`
+    padding: 0 4rem 4rem 0;
+
+    @media screen and (max-width: 1100px){
+        padding: 0 0 4rem 0;
+    }
+`
+
+const Heading = styled.h3`
+    font-size: 2rem;
+`
+
+const Description = styled.p`
+    line-height: 1.5;
+    font-size: clamp(1.3rem, 4vw, 1.5rem);
+    font-weight: 300;
+    padding: 2rem 0;
+`
+
+const ImgContent = styled(Img)`
+    height: 100%;
+    max-width: 100%;
+    position: relative;
+    border-radius: 10px;
+    padding: 4rem;
+`
+
+const ImgWrapper = styled.div`
+    @media screen and (max-width: 1100px){
+        height: 350px;
     }
 `
